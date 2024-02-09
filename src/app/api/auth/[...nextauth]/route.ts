33b359db73740a94690b6c7cf28dddba18e6da8c
Facundo Campodonico <facundocampodonico04@gmail.com>
2024-02-09 18:45:34 -0300

    configurando devuelta pero para que la autenticacion sea con google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,12 @@
-import { handlers } from "@/auth"
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google';
 
-export const {GET, POST} = handlers
+const handler = NextAuth({
+    providers: [ GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    ],
+});
+
+export { handler as GET, handler as Post };
